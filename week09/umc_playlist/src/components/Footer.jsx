513d40689a80a7454dd5102c69f7e1068cd12d8a
gpdnjs8 <jade3735@naver.com>
2024-11-18 15:41:09 +0900

    style: 스타일링 수정

--- week09/umc_playlist/src/components/Footer.jsx
+++ week09/umc_playlist/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = ()=> {
     return(
         <nav>
             <div style={{display: 'flex', justifyContent: 'center'}}>
-                <h3>University MakeuUs Challenge</h3>
+                <h3>University MakeUs Challenge</h3>
             </div>
         </nav>
     );
